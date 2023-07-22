782ea45693b8da5e994bb3503a6edb826b0382d5
eran-bolandian <eranbolan91@gmail.com>
2023-07-22 12:10:54 +0300

    upload files

---
+++ EranBolandian.java
@@ -0,0 +1,7 @@
+public class EranBolandian{
+	public static void main(String[] args){
+		for(int i=4;i>=0;i--){
+			System.out.println("Eran Bolandian");
+		}
+       }
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Ready to EXPLODE");
+		for(int i=10;i>=0;i--){
+			System.out.println("Count down:" +i);
+		} 
+		System.out.println("BOOOOOOMMMMM");
+       }
+}
